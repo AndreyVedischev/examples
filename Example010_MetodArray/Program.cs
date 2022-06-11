b53c8a9d992f5dd9b2b072d1d077c1318cc2af77
Andrey Vedischev <vedischev76@mail.ru>
2022-06-11 18:17:22 +0500

    добавили поиск элемента в массиве

---
+++ Example010_MetodArray/Program.cs
@@ -0,0 +1,13 @@
+﻿int [] array = {1,7,15,12,31,19,21,92,13};
+int n = array.Length;
+int finde = 12;
+
+int index = 0;
+while(index<n)
+{
+    if(array[index] == finde)
+    {
+        Console.WriteLine(index);
+    }
+    index++;
+}
