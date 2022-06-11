4576167c7b74758b32ae4f2aa66bbf92999ebfba
Andrey Vedischev <vedischev76@mail.ru>
2022-06-11 17:18:23 +0500

    добавил другое решение с функцией Мах

--- Example008_IntroMetod/Program.cs
+++ Example008_IntroMetod/Program.cs
@@ -12,12 +12,13 @@ int b1 = 26;
 int b2 = 15;
 int b3 = 42;
 int c1 = 228;
-int c2 = 61;
+int c2 = 617;
 int c3 = 37;
 
-int max1 = Max(a1, a2, a3);
-int max2 = Max(b1, b2, b3);
-int max3 = Max(c1, c2, c3);
-int max = Max(max1, max2, max3);
+//int max1 = Max(a1, a2, a3);
+//int max2 = Max(b1, b2, b3);
+//int max3 = Max(c1, c2, c3);
+//int max = Max(max1, max2, max3);
 
+int max = Max(Max(a1, a2, a3), Max(b1, b2, b3), Max(c1, c2, c3));
 Console.WriteLine(max);
