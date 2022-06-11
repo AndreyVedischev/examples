9063dfe043b89de8154b33a1ef7ff08f13afc2a7
Andrey Vedischev <vedischev76@mail.ru>
2022-06-11 16:25:17 +0500

    добавил простое решение

---
+++ Example008_IntroMetod/Program.cs
@@ -0,0 +1,21 @@
+﻿int a1 = 12;
+int a2 = 14;
+int a3 = 11;
+int b1 = 26;
+int b2 = 15;
+int b3 = 42;
+int c1 = 22;
+int c2 = 61;
+int c3 = 37;
+
+int max = a1;
+if(a2 >max) max = a2;
+if(a3 >max) max = a3;
+if(b1 >max) max = b1;
+if(b2 >max) max = b2;
+if(b3 >max) max = b3;
+if(c1 >max) max = c1;
+if(c2 >max) max = c2;
+if(c3 >max) max = c3;
+
+Console.WriteLine(max);
