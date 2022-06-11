59d792d574635c2c779d2f2ac4b1f6e650d49d87
Andrey Vedischev <vedischev76@mail.ru>
2022-06-11 18:55:59 +0500

    добавили методы войд для рандомного массива

---
+++ Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,26 @@
+﻿void FillArray(int[] collecion)
+{
+    int length = collecion.Length;
+    int index = 0;
+    while(index < length)
+    {
+        collecion[index] = new Random().Next(1,10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while(position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int [] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
