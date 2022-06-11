c65575e64cb961b68eaea10f4a1039084841035f
Andrey Vedischev <vedischev76@mail.ru>
2022-06-11 19:36:14 +0500

    добавили метод инт для поиска элемента

--- Example011_ArrayLibrary/Program.cs
+++ Example011_ArrayLibrary/Program.cs
@@ -1,15 +1,16 @@
-﻿void FillArray(int[] collecion)
+﻿void FillArray(int[] collection)
 {
-    int length = collecion.Length;
+    int length = collection.Length;
     int index = 0;
     while(index < length)
     {
-        collecion[index] = new Random().Next(1,10);
+        collection[index] = new Random().Next(1,10);
         index++;
     }
 }
 
 void PrintArray(int[] col)
+
 {
     int count = col.Length;
     int position = 0;
@@ -20,7 +21,28 @@ void PrintArray(int[] col)
     }
 }
 
+int IndexOf(int[] collection, int finde)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while(index < count)
+    {
+        if(collection[index] == finde)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+     
+}
 int [] array = new int[10];
 
 FillArray(array);
 PrintArray(array);
+Console.WriteLine("позиция");
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
