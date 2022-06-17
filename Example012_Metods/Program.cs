5411040a047cbcdae74581019496e7d5a4f4533b
Andrey Vedischev <vedischev76@mail.ru>
2022-06-17 22:03:31 +0500

    добавили метод для текста

--- Example012_Metods/Program.cs
+++ Example012_Metods/Program.cs
@@ -79,3 +79,26 @@ for(int i = 2; i <= 10; i++)
     }
     Console.WriteLine();
 }
+
+string text = "- Я думаю, - сказал князь, улыбаясь, - что, "
+            + "ежели бы вас послали вместо нашего милого Винценгероде, "
+            + "вы бы взяли приступом согласие прусского короля. "
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+
+// string s = "qwerty"
+//             0123
+// s[3] // r
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+    int length = text.Length;
+    for(int i = 0; i < length; i++)
+    {
+        if(text[i] == oldValue) result + $"{newValue}";
+        else result + $"{str[i]}";
+    }
+
+    return result;
+}
+string newText = Replace(text, ' ','|');
