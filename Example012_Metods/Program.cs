911bc793529dd38ae0ac2979dd468395e2849ef4
Andrey Vedischev <vedischev76@mail.ru>
2022-06-17 22:17:30 +0500

    добавили метод для текста с заменой символов

--- Example012_Metods/Program.cs
+++ Example012_Metods/Program.cs
@@ -71,14 +71,14 @@ string Method4(int count, string text)
 
   // цикл в цикле
 
-for(int i = 2; i <= 10; i++)
-{
-    for(int j = 2; j <= 10; j++)
-    {
-        Console.WriteLine($"{i} x {j} = {i*j}");
-    }
-    Console.WriteLine();
-}
+// for(int i = 2; i <= 10; i++)
+// {
+//     for(int j = 2; j <= 10; j++)
+//     {
+//         Console.WriteLine($"{i} x {j} = {i*j}");
+//     }
+//     Console.WriteLine();
+// }
 
 string text = "- Я думаю, - сказал князь, улыбаясь, - что, "
             + "ежели бы вас послали вместо нашего милого Винценгероде, "
@@ -95,10 +95,17 @@ string Replace(string text, char oldValue, char newValue)
     int length = text.Length;
     for(int i = 0; i < length; i++)
     {
-        if(text[i] == oldValue) result + $"{newValue}";
-        else result + $"{str[i]}";
+        if(text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
     }
 
     return result;
 }
 string newText = Replace(text, ' ','|');
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(newText, 'к','К');
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(newText, 'с','С');
+Console.WriteLine(newText);
